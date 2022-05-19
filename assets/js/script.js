ed0b16180619b8c7164292eb506c915cd3a69e8d
deb boro <debajit.boro666@gmail.com>
2022-05-18 20:30:18 -0400

    ability to submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,46 @@
-var buttonEl = document.querySelector('#save-task')
 var tasksToDoEl = document.querySelector('#tasks-to-do')
 var formEl = document.querySelector('#task-form')
 
-var createTaskHandler = function (event) {
-  event.preventDefault()
+var createTaskEl = function (testDataObj) {
+  //create list item
   var listItemEl = document.createElement('li')
   listItemEl.className = 'task-item'
-  listItemEl.textContent = 'This is a new task'
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement('div')
+  taskInfoEl.className = 'task-info'
+
+  //add html content to div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    testDataObj.name +
+    '</h3>' +
+    "<span class='task-type'>" +
+    testDataObj.task +
+    '</span>'
+  listItemEl.appendChild(taskInfoEl)
+
+  //add entire list item to list
   tasksToDoEl.appendChild(listItemEl)
 }
 
-//buttonEl.addEventListener('click', createTaskHandler)
-formEl.addEventListener('submit', createTaskHandler)
+var taskFormHandler = function (event) {
+  event.preventDefault()
+  var taskNameInput = document.querySelector("input[name='task-name']").value
+  var taskTypeInput = document.querySelector("select[name='task-type']").value
+
+  if (!taskNameInput || !taskTypeInput) {
+    alert('Please provide an input')
+  }
+
+  formEl.reset()
+
+  //data as object
+  var taskDataObj = {
+    name: taskNameInput,
+    task: taskTypeInput,
+  }
+  createTaskEl(taskDataObj)
+}
+
+formEl.addEventListener('submit', taskFormHandler)
